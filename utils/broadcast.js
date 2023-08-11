e1201ac7e02d57db1aa04226f59524902a417c7e
Achmad96 <totsukamail3278@gmail.com>
2023-08-11 12:46:31 +0700

    modified files

--- utils/broadcast.js
+++ utils/broadcast.js
@@ -8,27 +8,23 @@ module.exports = async (guild, useEmbeded = false, message) => {
   members.forEach(async member => {
     try {
       const name = typeof member.user.globalName === "string" ? member.user.globalName : member.user.username;
-      // if (member.user.id === "325435317081473026") {
-      const messages = `${message.replaceAll("[name]", `${name}`)}`;
-      if (useEmbeded) {
-        const embed = new EmbedBuilder()
-          .setColor(0x004e82)
-          .setAuthor({ name: client.user.username, iconURL: clientAvatar })
-          .setTitle("Schedule Time!")
-          .addFields({ name: "Pesan", value: messages + " :grin:" })
-          .setTimestamp()
-          .setFooter({ text: "Created at" });
-        member.send({ embeds: [embed] });
-      } else {
-        member.send(messages);
+      if (member.user.id === "325435317081473026") {
+        const messages = `${message.replaceAll("[name]", `${name}`)}`;
+        if (useEmbeded) {
+          const embed = new EmbedBuilder()
+            .setColor(0x004e82)
+            .setAuthor({ name: client.user.username, iconURL: clientAvatar })
+            .setTitle("Schedule Time!")
+            .addFields({ name: "Pesan", value: messages + " :grin:" })
+            .setTimestamp()
+            .setFooter({ text: "Created at" });
+          member.send({ embeds: [embed] });
+        } else member.send(messages);
+        const dmChannel = await member.user.createDM();
+        const msgs = await dmChannel.messages.fetch({ limit: 1 });
+        msgs.forEach(msg => msg.delete());
+        console.log(`Sent message to ${name}`);
       }
-      const dmChannel = await member.user.createDM();
-      const msgs = await dmChannel.messages.fetch({ limit: 1 });
-      console.log("🚀 ~ file: broadcast.js:28 ~ module.exports= ~ msgs:", msgs);
-      // setTimeout(() => msgs?.delete(), 5000);
-      console.log(`Sent message to ${name}`);
-      return;
-      // }
     } catch (error) {
       console.log("Error:", error.message);
     }
