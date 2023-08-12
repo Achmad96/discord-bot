f453302f6dc1e3d982d602af31e2a0568fd163a0
Achmad96 <totsukamail3278@gmail.com>
2023-08-13 06:53:58 +0700

    modified 04schedule.js

--- events/ready/04schedule.js
+++ events/ready/04schedule.js
@@ -3,13 +3,17 @@ const broadcast = require("../../utils/broadcast");
 
 const delayToDelete = 120000;
 
+const schedule = (date, callback, options) => {
+  cron.schedule(date, callback, options);
+};
+
 module.exports = client => {
   const guild = client.guilds.resolve("871691287865221120");
 
   if (!guild) return;
 
-  cron.schedule("00 7 * * *", () => broadcast(guild, true, "Selamat pagi [name]-kun!, selamat menjalankan aktivitas", delayToDelete));
-  cron.schedule("00 12 * * *", () => broadcast(guild, true, "Selamat siang [name]-kun!", delayToDelete));
-  cron.schedule("00 15 * * *", () => broadcast(guild, true, "Selamat sore [name]-kun!", delayToDelete));
-  cron.schedule("00 21 * * *", () => broadcast(guild, true, "Selamat malam [name]-kun!, jangan lupa tidur ya...", delayToDelete));
+  schedule("00 07 * * *", () => broadcast(guild, true, "Selamat pagi [name]-kun!, selamat menjalankan aktivitas", delayToDelete), { timezone: "Indonesia/Jakarta" });
+  schedule("00 12 * * *", () => broadcast(guild, true, "Selamat siang [name]-kun!", delayToDelete), { timezone: "Indonesia/Jakarta" });
+  schedule("00 15 * * *", () => broadcast(guild, true, "Selamat sore [name]-kun!", delayToDelete), { timezone: "Indonesia/Jakarta" });
+  schedule("00 21 * * *", () => broadcast(guild, true, "Selamat malam [name]-kun!, jangan lupa tidur ya...", delayToDelete), { timezone: "Indonesia/Jakarta" });
 };
