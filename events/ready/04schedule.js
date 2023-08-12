4c0fa5f199721da937b006a941e4925728663987
Achmad96 <totsukamail3278@gmail.com>
2023-08-12 12:01:54 +0700

    modified 04schedule.js

--- events/ready/04schedule.js
+++ events/ready/04schedule.js
@@ -7,6 +7,7 @@ module.exports = client => {
   if (!guild) return;
 
   const goodMorning = cron.schedule("00 7 * * *", () => broadcast(guild, true, "Selamat pagi [name]-kun!, selamat menjalankan aktivitas"));
+  const goodAfternoon = cron.schedule("00 12 * * *", () => broadcast(guild, true, "Selamat siang [name]-kun!"));
   const evening = cron.schedule("00 15 * * *", () => broadcast(guild, true, "Selamat sore [name]-kun!"));
   const night = cron.schedule("00 21 * * *", () => broadcast(guild, true, "Selamat malam [name]-kun!, jangan lupa tidur ya..."));
   listSchedule.push(goodMorning, evening, night);
