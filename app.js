6bdb62f8cb5ff37240965dcb6bf50db418f67b43
Achmad96 <totsukamail3278@gmail.com>
2023-09-17 15:58:55 +0700

    modified files

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const eventHandler = require("./handlers/eventHandler");
 const { Client, IntentsBitField, Partials, Collection } = require("discord.js");
 
 const client = new Client({
-  intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMembers, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.MessageContent, IntentsBitField.Flags.DirectMessages],
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+    IntentsBitField.Flags.DirectMessages,
+  ],
   partials: [Partials.Channel],
 });
 
